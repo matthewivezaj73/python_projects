64c09c4d5f0b1cb22f89f5b1ca6aefd3f542c4c0
matthewivezaj73 <81784356+matthewivezaj73@users.noreply.github.com>
2021-12-12 16:49:22 -0500

    added logic and check to see if file exists.

--- list_of_attendees/guests_main.py
+++ list_of_attendees/guests_main.py
@@ -44,15 +44,16 @@ while not not_guest:
     guest_list = [add_person, add_age, guest_phone, guestType]
     #Created a variable and assigned it a file path.
     my_file = "text_files/guest_info.txt"
-    #Opening a file for writing.
-    with open(my_file) as txt_file:
-        #Assigning the file path to a variable.
-        csv_file = "text_files/guest_info.csv"
-        #Checking if the csv file exists.
-        if os.isfile("text_files/guest_info.csv"): 
-            #Creating the csv file.
-            with open("text_files/guest_info.csv", "w+") as csv_file:
-                for line in guest_list:
-                    #Writing each guest info to the file.
-                    csv_file.write(line)
+    if os.path.isfile(my_file):
+        #Opening a file for writing.
+        with open(my_file,"w+") as txt_file:
+            #Assigning the file path to a variable.
+            csv_file = "text_files/guest_info.csv"
+            #Checking if the csv file exists.
+            if os.path.isfile("text_files/guest_info.csv"): 
+                #Creating the csv file.
+                with open("text_files/guest_info.csv", "w+") as csv_file:
+                    for line in guest_list:
+                        #Writing each guest info to the file.
+                        csv_file.write(line)
 
