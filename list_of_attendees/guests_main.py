7739027e71536a811ccd16cb060f72c03071b508
matthewivezaj73 <81784356+matthewivezaj73@users.noreply.github.com>
2021-12-12 16:32:02 -0500

    assigning the file path to a variable.

--- list_of_attendees/guests_main.py
+++ list_of_attendees/guests_main.py
@@ -38,5 +38,6 @@ while not not_guest:
         guestType = input("What is the type of guest?")
         #Validating the guest type.
         my_guest.guest_type(guestType)
+    my_file = "text_files/guest_info.txt"
     #Opening a file for writing.
-    with open("text_files/guest_info.txt") as txt_file:
+    with open("text_files/guest_info.txt", "w+") as txt_file:
