3e0f9845d9bcabdd63651e82101edccf7c2b6994
matthewivezaj73 <81784356+matthewivezaj73@users.noreply.github.com>
2021-12-12 21:43:04 -0500

    Removed a line.

--- list_of_attendees/guests_main.py
+++ list_of_attendees/guests_main.py
@@ -1,7 +1,6 @@
 #https://github.com/matthewivezaj73/python_projects/tree/main/list_of_attendees
 #Importing the validate_guests file and the Guests class.
 from classes.validate_guests import Guests
-from PIL import Image, ImageDraw, ImageFont
 
 #Importing the os library.
 import os
