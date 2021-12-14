70b12a36e7ecf607c1f42c044c16a18e202ddaef
matthewivezaj73 <81784356+matthewivezaj73@users.noreply.github.com>
2021-12-13 21:03:03 -0500

    Added logic to check numbers.

--- numbers.py
+++ numbers.py
@@ -28,19 +28,46 @@ for numbers in my_numbers:
             print(f"The number guessed is {my_numbers[10]}")
         #Handling the case where the user enters 6.
         elif numbers == 7:
-            print(f"The number guessed is {my_numbers[6]}")
+            print(f"The number guessed is {my_numbers[12]}")
         #Handling the case where the user enters 6.
         elif numbers == 8:
-            print(f"The number guessed is {my_numbers[8]}")
+            print(f"The number guessed is {my_numbers[14]}")
         #Handling the case where the user enters 6.
         elif numbers == 9:
-            print(f"The number guessed is {my_numbers[10]}")
+            print(f"The number guessed is {my_numbers[16]}")
         #Handling the case where the user enters 6.
         elif numbers == 10:
-            print(f"The number guessed is {my_numbers[9]}")
+            print(f"The number guessed is {my_numbers[18]}")
     elif numbers.isalpha():
         #Adding a breakpoint
         breakpoint()
         #Handling the case where the user enters One.
         if type(numbers) is str and numbers[0] == "O":
             print(f"The number guessed is {my_numbers[1]}")
+        #Handling the case where the user enters One.
+        if type(numbers) is str and numbers[0] == "T" and numbers[1] == "w":
+            print(f"The number guessed is {my_numbers[3]}")
+        #Handling the case where the user enters One.
+        if type(numbers) is str and numbers[0] == "T" and numbers[1] == "h":
+            print(f"The number guessed is {my_numbers[5]}")
+        #Handling the case where the user enters One.
+        if type(numbers) is str and numbers[0] == "F":
+            print(f"The number guessed is {my_numbers[7]}")
+        #Handling the case where the user enters One.
+        if type(numbers) is str and numbers[0] == "F" and numbers[1] == "i":
+            print(f"The number guessed is {my_numbers[9]}")
+        #Handling the case where the user enters One.
+        if type(numbers) is str and numbers[0] == "S" and numbers[0] == "i":
+            print(f"The number guessed is {my_numbers[11]}")
+        #Handling the case where the user enters One.
+        if type(numbers) is str and numbers[0] == "S" and numbers[1] == "e":
+            print(f"The number guessed is {my_numbers[13]}")
+        #Handling the case where the user enters One.
+        if type(numbers) is str and numbers[0] == "E":
+            print(f"The number guessed is {my_numbers[15]}")
+        #Handling the case where the user enters One.
+        if type(numbers) is str and numbers[0] == "N":
+            print(f"The number guessed is {my_numbers[17]}")
+        #Handling the case where the user enters One.
+        if type(numbers) is str and numbers[0] == "T":
+            print(f"The number guessed is {my_numbers[19]}")
