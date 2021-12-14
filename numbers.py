8f08c1c9a4699f239f9720168297b3aa307eb710
matthewivezaj73 <81784356+matthewivezaj73@users.noreply.github.com>
2021-12-13 20:44:46 -0500

    added a comment.

--- numbers.py
+++ numbers.py
@@ -4,6 +4,7 @@ my_numbers = [1,"One",2,"Two",3,"Three",4,"Four", 5, "Five", 6, "Six", 7, "Seven
 for numbers in my_numbers:
     if numbers.isdigit():
         print("The number is in numerical for")
+        #Handling the case where the user enters 1.
         if numbers == 1:
             print(f"The number is {my_numbers[0]}")
         elif numbers == 2:
