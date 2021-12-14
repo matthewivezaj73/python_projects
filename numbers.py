4323316674183a2ff4c1e387fda3cf8bb42dfb3b
matthewivezaj73 <81784356+matthewivezaj73@users.noreply.github.com>
2021-12-13 20:42:48 -0500

    added a print statement and a validation for #s

--- numbers.py
+++ numbers.py
@@ -2,3 +2,5 @@
 [1,"One",2,"Two",3,"Three",4,"Four", 5, "Five", 6, "Six", 7, "Seven", 8, "Eight", 9, "Nine", 10, "Ten"]
 #A for loop.
 for numbers in my_numbers:
+    if numbers.isdigit():
+        print("The number is in numerical for")
