33b6cff092c3d757cb8da6b1ad9e031e98d5abf6
matthewivezaj73 <81784356+matthewivezaj73@users.noreply.github.com>
2021-12-14 19:53:51 -0500

    Entered another user choice.

--- system.py
+++ system.py
@@ -7,7 +7,7 @@ user_info = []
 #Testing for user input.
 while not not_host:
     #Asking the user to make a choice.
-    user_choice = input("Please enter h if you would like to check the file directory this program is running from.\n")
+    user_choice = input("Please enter h if you would like to check the file directory this program is running from.\nPlease enter a if you would like to check the ip address of the system.\n")
     #handling the case where the user selects option h.
     if user_choice.lower() == "h":
         #Assigning a variable to the hostname.
