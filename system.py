5d4f67129178570a795988dbdf6232d37981d155
matthewivezaj73 <81784356+matthewivezaj73@users.noreply.github.com>
2021-12-14 19:49:28 -0500

    Entered a comment.

--- system.py
+++ system.py
@@ -4,6 +4,7 @@ import socket
 not_host = False
 #Testing for user input.
 while not not_host:
+    #Asking the user to make a choice.
     user_choice = input("Please enter h if you would like to check the file directory this program is running from.\n")
     #Assigning a variable to the hostname.
     hostname = socket.gethostname()
