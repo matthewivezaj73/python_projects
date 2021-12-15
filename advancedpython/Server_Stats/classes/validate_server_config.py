9540544c0582578993d61e8c5201921239315899
matthewivezaj73 <81784356+matthewivezaj73@users.noreply.github.com>
2021-12-15 07:41:45 -0500

    removed a blank line.

--- advancedpython/Server_Stats/classes/validate_server_config.py
+++ advancedpython/Server_Stats/classes/validate_server_config.py
@@ -8,7 +8,6 @@ class Server:
         self.server_type = server_type
         self.server_mem = server_mem
         self.server_hertz = server_hertz
-
     def get_ac(self):
         """
         A method that will tell you if the ac is plugged in or not.
