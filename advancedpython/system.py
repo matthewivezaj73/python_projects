c0c53a056ed0edbd6ba7be89c491d2e1bdb77799
matthewivezaj73 <81784356+matthewivezaj73@users.noreply.github.com>
2021-12-14 20:02:09 -0500

    adding a try except block

--- advancedpython/system.py
+++ advancedpython/system.py
@@ -8,16 +8,17 @@ user_info = []
 while not not_host:
     #Asking the user to make a choice.
     user_choice = input("Please enter \'h\' if you would like to check the file directory this program is running from.\nPlease enter \'a\' if you would like to check the ip address of the system.\n")
-    #Assigning a variable to the hostname.
-    hostname = socket.gethostname()
-    #handling the case where the user selects option h.
-    if user_choice.lower() == "h":
-        #Printing the hostname
-        print(hostname)
-    #Handling the case where the user selects option a.
-    elif user_choice.lower() == "a":
-        #Grabbing the ip address of the system and assigning it to a variable.
-        ipaddress = socket.getaddrinfo(hostname,None)
-        #Printing the ipaddress.
-        print(ipaddress)
-
+    try:
+        #handling the case where the user selects option h.
+        if user_choice.lower() == "h":
+            #Assigning a variable to the hostname.
+            hostname = socket.gethostname()
+            #Printing the hostname
+            print(hostname)
+        #Handling the case where the user selects option a.
+        elif user_choice.lower() == "a":
+            #Grabbing the ip address of the system and assigning it to a variable.
+            ipaddress = socket.getaddrinfo(hostname,None)
+            #Printing the ipaddress.
+            print(ipaddress)
+    except:
