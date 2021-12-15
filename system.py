57392a98f9f8da89847ac5e1bb3cf57f6ed7f201
matthewivezaj73 <81784356+matthewivezaj73@users.noreply.github.com>
2021-12-14 19:51:12 -0500

    Entered in a case if the user chooses h.

--- system.py
+++ system.py
@@ -7,6 +7,6 @@ while not not_host:
     #Asking the user to make a choice.
     user_choice = input("Please enter h if you would like to check the file directory this program is running from.\n")
     #handling the case where the user selects option h.
-    
-    #Assigning a variable to the hostname.
-    hostname = socket.gethostname()
+    if user_choice.lower() == "h":
+        #Assigning a variable to the hostname.
+        hostname = socket.gethostname()
