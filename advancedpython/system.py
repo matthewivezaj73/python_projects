ac85a7ba4c181b0e8e2b940fcb20e747c4649b7e
matthewivezaj73 <81784356+matthewivezaj73@users.noreply.github.com>
2021-12-14 20:28:47 -0500

    removed initial try and moved it .

--- advancedpython/system.py
+++ advancedpython/system.py
@@ -9,37 +9,37 @@ user_info = []
 while not not_host:
     #Asking the user to make a choice.
     user_choice = input("Please enter \'h\' if you would like to check the file directory this program is running from.\nPlease enter \'a\' if you would like to check the ip address of the system.\nPlease enter \'s\' if you would like to check cpu stats.\nPlease enter \'p\' if you would like to check the cpu percent.\nPlease enter \'e\' if you would like to exit\nPlease enter your choice here: ")
-    #Try block
-    try:
-        #handling the case where the user selects option h.
-        if user_choice.lower() == "h":
-            #Assigning a variable to the hostname.
-            hostname = socket.gethostname()
-            #Printing the hostname
-            print(hostname)
-        #Handling the case where the user selects option a.
-        elif user_choice.lower() == "a":
+    #handling the case where the user selects option h.
+    if user_choice.lower() == "h":
+        #Assigning a variable to the hostname.
+        hostname = socket.gethostname()
+        #Printing the hostname
+        print(hostname)
+    #Handling the case where the user selects option a.
+    elif user_choice.lower() == "a":
+        try:
             #Grabbing the ip address of the system and assigning it to a variable.
             ipaddress = socket.getaddrinfo(hostname,None)
             #Printing the ipaddress.
             print(ipaddress)
-        #Handling the case where the user selects option s.
-        elif user_choice.lower() == "s":
-            #Grabbing the cpu stats and assigning them to a variable.
-            cpustats = psutil.cpu_stats()
-            #Printing the cpu stats.
-            print(cpustats)
-        #Handling the case where the user selects option p.
-        elif user_choice.lower() == "p":
-            #Assigning the cpu percentage to a variable.
-            cpupercent = psutil.cpu_percent(interval=0.01)
-            #Printing the cpu percentage.
-            print(cpupercent)
-        #Handling the case where the user selects option p.
-        elif user_choice.lower() == "3":
-            #Setting the not_host flag to true.
-            not_host = True
-    #Handling the case where the cases above are not met.
-    except:
-        print("That choice was either invalid or you must first grab the host name by entering \'h\'")
+                #Handling the case where the cases above are not met.
+        except:
+            print("That choice was either invalid or you must first grab the host name by entering \'h\'")
+    #Handling the case where the user selects option s.
+    elif user_choice.lower() == "s":
+        #Grabbing the cpu stats and assigning them to a variable.
+        cpustats = psutil.cpu_stats()
+        #Printing the cpu stats.
+        print(cpustats)
+    #Handling the case where the user selects option p.
+    elif user_choice.lower() == "p":
+        #Assigning the cpu percentage to a variable.
+        cpupercent = psutil.cpu_percent(interval=0.01)
+        #Printing the cpu percentage.
+        print(cpupercent)
+    #Handling the case where the user selects option p.
+    elif user_choice.lower() == "3":
+        #Setting the not_host flag to true.
+        break
+
 
