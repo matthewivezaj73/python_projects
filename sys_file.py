3431e52983817d113914785942fa0ac7966de387
matthewivezaj73 <81784356+matthewivezaj73@users.noreply.github.com>
2021-12-17 07:37:47 -0500

    Changed the wording in a comment.

--- sys_file.py
+++ sys_file.py
@@ -92,7 +92,7 @@ while not not_done:
         not_done = True
         #Printing a message to the user.
         print("Now exiting the system....")
-    #Handling the case where the user enters e.
+    #Handling the case where the user enters ac.
     elif user_choice.lower() == "ac":
         ac_connected()
 
