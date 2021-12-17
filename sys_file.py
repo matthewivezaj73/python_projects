760e1c658782475dfe7e4a55b1ea32e9f2a0ca30
matthewivezaj73 <81784356+matthewivezaj73@users.noreply.github.com>
2021-12-16 21:36:11 -0500

    Replaced the code with what was in book

--- sys_file.py
+++ sys_file.py
@@ -1,90 +1,43 @@
 #!/usr/bin/env python
 # coding: utf-8
-
-#The first line defines the interpreter 
-# and the second line defines the encoding for the project.
-
-#Importing the following libraries:
-# - socket
-# - sys
-# - psutil
 import socket
 import sys
 import psutil
-#Assigning help text to a variable.
 HELP_TEXT = """usage: python {program_name:s}
 Displays the values of the sensors
 Options and arguments:
 --help:    Display this message"""
-################## ---------------------- Begin adding functions. ---------------------- ##################
-def check_power():
-    """
-    A function that checks to 
-    see if the power is connected on a system.
-
-    Function used:
-    - sensors_battery
-    """
-    return psutil.sensors_battery().power_plugged
-def command_line(argv):
-    """
-    A function that brings up a command line to the user.
-    """
-    #Starting a try block.
-    try:
-        program_name, *arguments = sys.argv
-        if not arguments:
-            show_sensors()
-        elif arguments and arguments[0] == '--help':
-            print(HELP_TEXT.format(program_name=program_name))
-            return
-        else:
-            print("Sorry, we can't do that.")
-    except ValueError:
-        print("Unknown arguments {}".format(arguments))
-def get_ram():
-    """
-    A function that gets the ram from a system
-    and returns it to the user.
-
-    function used:
-    - virtual_memory
-    """
-    return psutil.virtual_memory().available
-def get_sys_info():
-    """
-    A function that gets the following information
-    and then returns them to the user to view:
-    - hostname
-    - addresses
-    """
-    hostname = socket.gethostname
+def python_version():
+    return sys.version_info
+def ip_addresses():
+    hostname = socket.gethostname()
+    addresses = socket.getaddrinfo(socket.gethostname(), None)
     address_info = []
-    sys_addresses = socket.getaddrinfo(socket.gethostname(), None)
-    for address in sys_addresses:
+    for address in addresses:
         address_info.append((address[0].name, address[4][0]))
-def load_sys():
-    return psutil.cpu_percent(interval=0.2)
-def show_version():
-    """
-    A function that prints out system version information.
-    """
-    sys_version = sys.version_info
-    return sys_version
+    return address_info
+def cpu_load():
+    return psutil.cpu_percent(interval=0.1)
+def ram_available():
+    return psutil.virtual_memory().available
+def ac_connected():
+    return psutil.sensors_battery().power_plugged
 def show_sensors():
-    """
-    A method that will show the sensor on a system.
-    """
-    print("Python version: {0.major}.{0.minor}".format(show_version()))
-    for address in get_sys_info():
+    print("Python version: {0.major}.{0.minor}".format(python_version()))
+    for address in ip_addresses():
         print("IP addresses: {0[1]} ({0[0]})".format(address))
-    print("CPU Load: {.1f}".format(load_sys()))
-    print("RAM Available: {} MiB".format(get_ram() / 1024**2))
-    print("AC Connected: {}".format(check_power()))
+    print("CPU Load: {:.1f}".format(cpu_load()))
+    print("RAM Available: {} MiB".format(ram_available() / 1024**2))
+    print("AC Connected: {}".format(ac_connected()))
+def command_line(argv):
+    program_name, *arguments = argv
+    if not arguments:
+        show_sensors()
+    elif arguments and arguments[0] == '--help':
+        print(HELP_TEXT.format(program_name=program_name))
+        return
+    else:
+        raise ValueError("Unknown arguments {}".format(arguments))
 
-################## ---------------------- Ending adding functions. ---------------------- ##################
-
-
-#Using the ifmain construct.
 if __name__ == '__main__':
     command_line(sys.argv)
