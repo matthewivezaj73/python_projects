591a6bafa4dcb576e9c0dc87b7fdec4ef51fcc58
matthewivezaj73 <81784356+matthewivezaj73@users.noreply.github.com>
2021-12-17 07:39:36 -0500

    Put the functions in order.

--- sys_file.py
+++ sys_file.py
@@ -7,54 +7,12 @@ HELP_TEXT = """usage: python {program_name:s}
 Displays the values of the sensors
 Options and arguments:
 --help:    Display this message"""
-def python_version():
-    """
-    A function that checks the version of python you are running.
-    """
-    return sys.version_info
-def ip_addresses():
-    """
-    A function the gets the hostname, IP address 
-    information, and appends everything to a list.
-    """
-    hostname = socket.gethostname()
-    addresses = socket.getaddrinfo(socket.gethostname(), None)
-    address_info = []
-    for address in addresses:
-        address_info.append((address[0].name, address[4][0]))
-    return address_info
-def cpu_load():
-    """
-    A function that checks the total amount of ram available 
-    on the system and displays it within intercals of a tenth of a percent.
-    """
-    return psutil.cpu_percent(interval=0.1)
-def ram_available():
-    """
-    A function that shows the user the 
-    total amount of memory available on a system.
-    """
-    return psutil.virtual_memory().available
 def ac_connected():
     """
     A function that checks to see if the power is connected 
     to the system. If it is, the function will return true.
     """
     return psutil.sensors_battery().power_plugged
-def show_sensors():
-    """
-    A function the displays the following:
-        - cpu internval
-        - Ram available
-        - ac connection status
-        - IP addresses
-    """
-    print("Python version: {0.major}.{0.minor}".format(python_version()))
-    for address in ip_addresses():
-        print(f"IP addresses: {address})")
-    print(f"CPU Load: {cpu_load()}")
-    print("RAM Available: {} MiB".format(ram_available() / 1024**2))
-    print("AC Connected: {}".format(ac_connected()))
 def command_line(argv):
     """
     A function that runs other user 
@@ -70,6 +28,48 @@ def command_line(argv):
             return
     except ValueError:
         print("Unknown arguments {}".format(arguments))
+def cpu_load():
+    """
+    A function that checks the total amount of ram available 
+    on the system and displays it within intercals of a tenth of a percent.
+    """
+    return psutil.cpu_percent(interval=0.1)
+def ip_addresses():
+    """
+    A function the gets the hostname, IP address 
+    information, and appends everything to a list.
+    """
+    hostname = socket.gethostname()
+    addresses = socket.getaddrinfo(socket.gethostname(), None)
+    address_info = []
+    for address in addresses:
+        address_info.append((address[0].name, address[4][0]))
+    return address_info
+def python_version():
+    """
+    A function that checks the version of python you are running.
+    """
+    return sys.version_info
+def ram_available():
+    """
+    A function that shows the user the 
+    total amount of memory available on a system.
+    """
+    return psutil.virtual_memory().available
+def show_sensors():
+    """
+    A function the displays the following:
+        - cpu internval
+        - Ram available
+        - ac connection status
+        - IP addresses
+    """
+    print("Python version: {0.major}.{0.minor}".format(python_version()))
+    for address in ip_addresses():
+        print(f"IP addresses: {address})")
+    print(f"CPU Load: {cpu_load()}")
+    print("RAM Available: {} MiB".format(ram_available() / 1024**2))
+    print("AC Connected: {}".format(ac_connected()))
     #End of try block
 #Setting a flag.
 not_done = False
