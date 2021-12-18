09b4472c53b8ed1997fbd56e39675fe76870646c
matthewivezaj73 <81784356+matthewivezaj73@users.noreply.github.com>
2021-12-18 14:51:17 -0500

    added a comment.

--- sys_file.py
+++ sys_file.py
@@ -109,6 +109,7 @@ while not not_done:
                 with open("sensor_data.csv","w+") as sensor_data:
                     for line in show_sensor_data:
                         print(line)
+            #Handling the alternative case.
     #Handling the case where the user enters cl.
     elif user_choice.lower() == "cl":
         #Calling the command_line function
