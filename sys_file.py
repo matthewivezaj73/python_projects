3588449f1b8caa5ec44811b50fc115bd4edb557f
matthewivezaj73 <81784356+matthewivezaj73@users.noreply.github.com>
2021-12-18 10:09:01 -0500

    Printing out the lines from show sensordata.

--- sys_file.py
+++ sys_file.py
@@ -83,6 +83,7 @@ while not not_done:
     if user_choice.lower() == "ss":
         #Calling the show sensors function
         show_sensor_data = show_sensors()
+        #Printing out show_sensor_data.
         #Asking the user if they would like to record the data on an excel sheet.
         record_response = input("Would you like to record the data on a spreadsheet? Y/N")
         #Handling the case where the user enters y for yes.
