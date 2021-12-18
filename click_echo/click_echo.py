932e0c746cac3987df6d657f2acc2b28a6389cf1
matthewivezaj73 <81784356+matthewivezaj73@users.noreply.github.com>
2021-12-18 16:01:12 -0500

    added an input variable.

--- click_echo/click_echo.py
+++ click_echo/click_echo.py
@@ -8,4 +8,6 @@ while not not_done:
     do_something = input("Please enter \'csv\' if you would like to create a csv file.\n\n")
     #Handling the case where the user enters csv.
     if do_something.lower() == "csv":
+        #Asking the user to input text.
+        content = input("Please enter the text that you would like to enter into the csv file for the first row: ")
         click.echo("processing your csv file")
