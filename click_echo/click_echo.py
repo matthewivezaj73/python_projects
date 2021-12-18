906c9491a8f21c025845e3b5f84bdc5959820d90
matthewivezaj73 <81784356+matthewivezaj73@users.noreply.github.com>
2021-12-18 15:33:39 -0500

    Giving the user a choice.

--- click_echo/click_echo.py
+++ click_echo/click_echo.py
@@ -5,4 +5,4 @@ not_done = False
 #Testing for user input.
 while not not_done:
     #Asking the user to enter a choice.
-    do_something = input("")
+    do_something = input("Please enter csv if you would like to create a csv file.\n\n")
