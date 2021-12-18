f4f44fcc85e4976e730f5e59be1cf1a506616149
matthewivezaj73 <81784356+matthewivezaj73@users.noreply.github.com>
2021-12-18 15:03:16 -0500

    added a comment.

--- sys_file.py
+++ sys_file.py
@@ -109,6 +109,7 @@ while not not_done:
             #Handling the case if None is found in the list.
             if None in my_data_list:
                 my_data_list.remove(None)
+            #Adding an alternative case.
             else:
                 print("This value looks good.")
         #Handling the case where the user enters y for yes.
