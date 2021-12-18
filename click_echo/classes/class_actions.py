d007ead7c95a5382983e0165a0fa1c9e4242d464
matthewivezaj73 <81784356+matthewivezaj73@users.noreply.github.com>
2021-12-18 17:45:34 -0500

    Created a doc string.

--- click_echo/classes/class_actions.py
+++ click_echo/classes/class_actions.py
@@ -1,5 +1,8 @@
 class Metrics:
     def __init__(self,metric_name,metric_value,quantity_of_metrics):
         """
-        
+        Initializing the following attributes.
+            - metric_name
+            - metric_value
+            - quantity_of_metrics
         """
