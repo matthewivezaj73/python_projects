56281583a729350cf50efca67040ea163ab4cb9b
matthewivezaj73 <81784356+matthewivezaj73@users.noreply.github.com>
2021-12-18 15:01:35 -0500

    added code to execute in case the user selects y.

--- sys_file.py
+++ sys_file.py
@@ -108,13 +108,15 @@ while not not_done:
             my_data_list.append(data)
         #Handling the case where the user enters y for yes.
         if record_response.lower() == "y":
-
-        #Handling the alternative case.
-            else:
-                #Opening the csv file for writing.
-                with open("sensor_data.csv","w+") as sensor_data:
-                    for line in show_sensor_data:
-                        print(line)
+            #Opening the csv file for writing.
+            with open("sensor_data.csv","w") as sensor_data:
+                for line in show_sensor_data:
+                    print(line)        #Handling the alternative case.
+        else:
+            #Opening the csv file for writing.
+            with open("sensor_data.csv","w+") as sensor_data:
+                for line in show_sensor_data:
+                    print(line)
             
     #Handling the case where the user enters cl.
     elif user_choice.lower() == "cl":
