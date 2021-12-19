d6af890298cfc70ef8e9b6755d228913acf13774
matthewivezaj73 <81784356+matthewivezaj73@users.noreply.github.com>
2021-12-19 14:22:51 -0500

    Modifiying do strings.

--- music_box/test_cases/music_test.py
+++ music_box/test_cases/music_test.py
@@ -12,14 +12,14 @@ class TestMusic(unittest.TestCase):
         self.new_music = ValidateMusic("Mike L", "Omega", "Sprigal")
     def test_validate_album_false(self):
         """
-        A method that will ensure that validate_artist returns false.
+        A method that will ensure that validate_album returns false.
         """
         invalid_artist_list = ["R0cky!", "!R0cky", "R0*cky!", "#R0cky!", " Rocky", "Rocky "," Rocky ", " ", "   ","&*@*$&#$","%","%*","1_2","                 "]
         for artist in invalid_artist_list:
             self.new_music.validate_album(artist)
     def test_validate_album_true(self):
         """
-        A method that will ensure that validate_artist returns true.
+        A method that will ensure that validate_album returns true.
         """
         invalid_artist_list = ["R0cky","Monty", "1Monty", "Monty3","A$AP R0CKY","Monty$","$Monty","$Monty$"]
         for artist in invalid_artist_list:
