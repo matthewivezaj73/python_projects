0e56341530c3a5f45d7ce311b5164aaa98e8d791
matthewivezaj73 <81784356+matthewivezaj73@users.noreply.github.com>
2021-12-19 13:18:46 -0500

    Removed a choice.

--- music_box/music_box_main.py
+++ music_box/music_box_main.py
@@ -9,7 +9,7 @@ playlist = []
 #Testing for user input.
 while not not_done:
     #Asking the user to enter a choice.
-    user_action = input("Enter \'a\' to add an entry.\n\nEnter \'s\' to show all entries.\n\nEnter \'e\' to edit an entry.\n\nEnter \'x\' to exit.")
+    user_action = input("Enter \'a\' to add an entry.\n\nEnter \'s\' to show all entries.\n\nEnter \'x\' to exit.")
     #Handling the case where the user enters a.
     if user_action.lower() == "a":
         #Creating a flag.
