9429d11037d6d37f6efc18d6a040a533779d1696
matthewivezaj73 <81784356+matthewivezaj73@users.noreply.github.com>
2021-12-19 12:47:20 -0500

    Added the imports and started my setup.

---
+++ music_box/test_cases/music_test.py
@@ -0,0 +1,12 @@
+#Importing the unittest module.
+import unittest
+#Importing the Guests module.
+from classes.validate_music_box import ValidateMusic
+#Creating the class.
+class TestMusic(unittest.TestCase):
+    def __init__(self):
+        """
+        Creating an instance of the Validate Music class.
+        """
+        #Creating an instance of the class.
+        new_music = ValidateMusic("Mike L", "Omega", "Sprigal")
