aa76d19806622f5b8654246a5896c83960aa2549
matthewivezaj73 <81784356+matthewivezaj73@users.noreply.github.com>
2021-12-19 10:09:54 -0500

    entered another choice.

--- music_box/music_box_main.py
+++ music_box/music_box_main.py
@@ -5,4 +5,4 @@ not_done = False
 #Testing for user input.
 while not not_done:
     #Asking the user to enter a choice.
-    user_action = input("Enter \'a\' to add an entry.\n\nEnter \'s\' to show all entries.\n\nEnter \'e\' to edit an entry.\n\n")
+    user_action = input("Enter \'a\' to add an entry.\n\nEnter \'s\' to show all entries.\n\nEnter \'e\' to edit an entry.\n\nEnter \'x\' to exit.")
