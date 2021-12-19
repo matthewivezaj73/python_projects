4ceda7f15e4da4dd8214cdf331ee09f0535a5429
matthewivezaj73 <81784356+matthewivezaj73@users.noreply.github.com>
2021-12-19 09:37:53 -0500

    Created doc string and added attributes.

--- music_box/classes/validate_music_box.py
+++ music_box/classes/validate_music_box.py
@@ -3,4 +3,7 @@ class ValidateMusic:
     Created a class that represents a 
     validator for the music content entered.
     """
-    def __init__(self,artist_name,album_name,)
+    def __init__(self,artist_name,album_name,song_title):
+        """
+        Creating a main method that initilzes the attributes.
+        """
