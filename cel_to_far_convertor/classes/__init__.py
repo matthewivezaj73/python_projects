3faf5edfc0f60c0a54788bb6111d2646306ddb36
matthewivezaj73 <81784356+matthewivezaj73@users.noreply.github.com>
2021-12-20 08:11:09 -0500

    Added a choice for the user.

--- cel_to_far_convertor/classes/__init__.py
+++ cel_to_far_convertor/classes/__init__.py
@@ -5,4 +5,4 @@ not_done = False
 #Testing for user input.
 while not not_done:
     #Asking the user what they would like to do.
-    
+    user_choice = input("Please enter cf if you would like to convert Celsius to Fahrenheit.")
