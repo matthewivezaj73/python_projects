987be849ab5382f5c3645354d3e7994df4aa00f5
matthewivezaj73 <81784356+matthewivezaj73@users.noreply.github.com>
2021-12-21 15:49:33 -0500

    Added attributes

--- monthly_spending/classes/spending.py
+++ monthly_spending/classes/spending.py
@@ -2,4 +2,4 @@ class Spending:
     """
     Created a class to represent
     """
-    def __init__(self,account_name,account_remaining,)
+    def __init__(self,account_name,account_remaining,account_life,account_increase,account_decrease,account_closure)
