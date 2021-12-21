629abb4158b074196759228feb605cb9bd9997a1
matthewivezaj73 <81784356+matthewivezaj73@users.noreply.github.com>
2021-12-21 15:23:36 -0500

    assed single quotes to help distinguish choice.

--- cel_to_far_convertor/cel_to_fah_main.py
+++ cel_to_far_convertor/cel_to_fah_main.py
@@ -9,7 +9,7 @@ not_done = False
 #Testing for user input.
 while not not_done:
     #Asking the user to make a choice.
-    user_choice = input("Please enter cf if you would like to convert Celsius to Fahrenheit.")
+    user_choice = input("Please enter \'cf\' if you would like to convert Celsius to Fahrenheit.")
     #Handling the case where the user enters cf.
     if user_choice.lower() == "cf":
         #Setting a flag.
