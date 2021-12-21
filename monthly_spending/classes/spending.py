1d8bed975769881e748594f495223103e4a92d7b
matthewivezaj73 <81784356+matthewivezaj73@users.noreply.github.com>
2021-12-21 15:55:24 -0500

    initialized attributes.

--- monthly_spending/classes/spending.py
+++ monthly_spending/classes/spending.py
@@ -11,4 +11,10 @@ class Spending:
             - account_increase
             - account_decrease
             - account_closure
-        """
+        """            
+        self.account_name = account_name
+        self.account_remaining = account_remaining
+        self.account_life = account_life
+        self.account_increase = account_increase
+        self.account_decrease = account_decrease
+        self.account_closure = account_closure
