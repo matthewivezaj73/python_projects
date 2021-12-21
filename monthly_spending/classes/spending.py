5891e855550bb2cff87d9328e181ae64ddb8e82d
matthewivezaj73 <81784356+matthewivezaj73@users.noreply.github.com>
2021-12-21 15:50:16 -0500

    Adding doc string

--- monthly_spending/classes/spending.py
+++ monthly_spending/classes/spending.py
@@ -2,4 +2,7 @@ class Spending:
     """
     Created a class to represent
     """
-    def __init__(self,account_name,account_remaining,account_life,account_increase,account_decrease,account_closure)
+    def __init__(self,account_name,account_remaining,account_life,account_increase,account_decrease,account_closure):
+        """
+        
+        """
