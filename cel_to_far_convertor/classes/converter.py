c1d9f4d0a65c43b71cd9ab6e0c79950df0658af0
matthewivezaj73 <81784356+matthewivezaj73@users.noreply.github.com>
2021-12-21 07:45:14 -0500

    Typecasting the str into type int.

--- cel_to_far_convertor/classes/converter.py
+++ cel_to_far_convertor/classes/converter.py
@@ -12,9 +12,9 @@ class converter:
         Takes in a value in celsius and converts it into fahrenheit.
         """
         if ("." in celsius) and (celsius.replace('.','')).isdigit() or celsius.isdigit():
-            return celsius * 9 / 5 + 32
+            return int(celsius) * 9 / 5 + 32
     def celcius_kelvin_converter(self,celsius):
         """
         Takes in a value in celsius and converts it into kelvin.
         """
-        return 273.15 + celsius
+        return 273.15 + int(celsius)
