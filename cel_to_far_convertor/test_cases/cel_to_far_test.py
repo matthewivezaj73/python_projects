964cb13c905fe1daa5fbe0bceac45a84c7e032d9
matthewivezaj73 <81784356+matthewivezaj73@users.noreply.github.com>
2021-12-21 07:53:56 -0500

    Created a test file.

---
+++ cel_to_far_convertor/test_cases/cel_to_far_test.py
@@ -0,0 +1,11 @@
+#Importing the unittest module.
+import unittest
+#Importing the converter module.
+from classes.converter import converter
+#Creating the class.
+class TestCrypto(unittest.TestCase):
+    def __init__(self):
+        """
+        Creating an instance of the Converter class.
+        """
+        
