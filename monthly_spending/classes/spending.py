f5eece515e8a875a18b085129acdacda323f1ace
matthewivezaj73 <81784356+matthewivezaj73@users.noreply.github.com>
2021-12-21 16:27:17 -0500

    changed method name.

--- monthly_spending/classes/spending.py
+++ monthly_spending/classes/spending.py
@@ -18,7 +18,7 @@ class Spending:
         self.account_increase = account_increase
         self.account_decrease = account_decrease
         self.account_closure = account_closure
-    def add_amount(self,amount_to_add):
+    def validate_amount(self,amount_to_add):
         """
         A method that takes the amount a user wants to add
         to the database.
