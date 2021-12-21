25768e95641dad5ff794c61bb9bffdc8e9b80eb8
matthewivezaj73 <81784356+matthewivezaj73@users.noreply.github.com>
2021-12-21 07:55:46 -0500

    Created a method for testing.

--- cel_to_far_convertor/test_cases/cel_to_far_test.py
+++ cel_to_far_convertor/test_cases/cel_to_far_test.py
@@ -10,3 +10,7 @@ class TestConverter(unittest.TestCase):
         """
         #Creating an instance of the class.
         my_converter = converter(98.33)
+    def test_celcius_fahrenheit_converter_false(self):
+        """
+        
+        """
