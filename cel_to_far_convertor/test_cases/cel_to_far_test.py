27fafd2231b62a707bbab690e20ff4b59c13d8d2
matthewivezaj73 <81784356+matthewivezaj73@users.noreply.github.com>
2021-12-21 07:54:22 -0500

    Renamed the testing class.

--- cel_to_far_convertor/test_cases/cel_to_far_test.py
+++ cel_to_far_convertor/test_cases/cel_to_far_test.py
@@ -3,7 +3,7 @@ import unittest
 #Importing the converter module.
 from classes.converter import converter
 #Creating the class.
-class TestCrypto(unittest.TestCase):
+class TestConverter(unittest.TestCase):
     def __init__(self):
         """
         Creating an instance of the Converter class.
