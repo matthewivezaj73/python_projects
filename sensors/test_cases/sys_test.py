6b5f2720da80e908818349cbbb9b6aea4e67deef
matthewivezaj73 <81784356+matthewivezaj73@users.noreply.github.com>
2021-12-22 07:30:48 -0500

    Created a docstring.

--- sensors/test_cases/sys_test.py
+++ sensors/test_cases/sys_test.py
@@ -4,3 +4,7 @@ import unittest
 from functions.sys_functions import *
 #Creating the class.
 class TestCrypto(unittest.TestCase):
+    """
+    A representation of test cases written 
+    to evaluate the functions in a program.
+    """
