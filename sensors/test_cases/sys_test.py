8e4471705c5987829187b6effe75fb40c31ba7f0
matthewivezaj73 <81784356+matthewivezaj73@users.noreply.github.com>
2021-12-22 07:33:18 -0500

    Added a setUp.

--- sensors/test_cases/sys_test.py
+++ sensors/test_cases/sys_test.py
@@ -8,6 +8,8 @@ class TestCrypto(unittest.TestCase):
     A representation of test cases written 
     to evaluate the functions in a program.
     """
+    def setUp(self):
+        
     def test_sys_ac_connected_assert_False(self):
         """
         A test to see if the ac_connected function will evaluate to false.
