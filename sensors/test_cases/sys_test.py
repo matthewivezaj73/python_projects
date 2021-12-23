7de95c71fa5c9a6b7f220a45f9b21a5e30039dea
matthewivezaj73 <81784356+matthewivezaj73@users.noreply.github.com>
2021-12-22 20:14:25 -0500

    changed doc string contents

--- sensors/test_cases/sys_test.py
+++ sensors/test_cases/sys_test.py
@@ -24,17 +24,19 @@ class TestCrypto(unittest.TestCase):
         self.assertTrue(ac_connected())
     def test_sys_command_line_assert_NotIn(self):
         """
-        A test to see if the ac_connected function will evaluate to false.
+        A test to see if the ac_connected function will 
+        
+        assert a value is can not be run inside of it.
 
-        Test used: assert false.
+        Test used: assertIn.
         """
         argv_list = ["12", "afd", "Ajs", "AJS", "dsad123", "FSD76", "HJjh234","2sd4","g231h","G342J","1HJK6"]
         for arg in argv_list:
-            self.assertFalse(command_line(arg))
+            self.assertNotIn(command_line(arg))
     def test_sys_command_line_assert_In(self):
         """
         A test to see if the ac_connected function will evaluate to true.
 
-        Test used: assert true.
+        Test used: assertNotIn.
         """
         self.assertIn(command_line())
