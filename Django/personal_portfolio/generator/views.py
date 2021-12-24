71d6c1a1b1012835c346374d22ab454bb7ca18e2
matthewivezaj73 <81784356+matthewivezaj73@users.noreply.github.com>
2021-12-24 14:42:56 -0500

    remoed files.

--- Django/personal_portfolio/generator/views.py
+++
@@ -1,9 +0,0 @@
-from django.shortcuts import render
-from django.http import HttpResponse
-import random
-# Create your views here.
-def home(request):
-    """
-    A function that returns text back to the user.
-    """
-    return render(request,'generator/home.html', )
