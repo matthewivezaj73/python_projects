f053419e04f3f1ee2f56fb1b5c93ae9ecc095f90
matthewivezaj73 <81784356+matthewivezaj73@users.noreply.github.com>
2021-12-30 14:41:58 -0500

    Added a comment.

--- pygame_projects/2_drawing_on_display.py
+++ pygame_projects/2_drawing_on_display.py
@@ -16,6 +16,7 @@ pygame.display.set_caption("Drawing objects")
 not_running = False
 #Starting a while loop
 while not not_running:
+    #Running through each event in event.
     for event in pygame.event.get():
         #Checking if the event types happening are equal to what is happening in pygame.
         if event.type == pygame():
