3c0e6a1bd2e3e623503e06ff5afb387e80eba775
matthewivezaj73 <81784356+matthewivezaj73@users.noreply.github.com>
2021-12-30 14:27:59 -0500

    Added a caption

--- pygame_projects/surface/1_display_surface.py
+++ pygame_projects/surface/1_display_surface.py
@@ -12,6 +12,8 @@ pygame.init()
 WINDOW_WIDTH = 600
 WINDOW_HEIGHT = 300
 display_surface = pygame.display.set_mode([WINDOW_WIDTH, WINDOW_HEIGHT])
+
+pygame.display.set_caption("Hello world!")
 #Creating a flag.
 not_looping = False
 #The main loop for the game
