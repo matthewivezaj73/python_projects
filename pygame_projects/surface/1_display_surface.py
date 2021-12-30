7962297ba1edef99265dc2667ee9626aeda0c880
matthewivezaj73 <81784356+matthewivezaj73@users.noreply.github.com>
2021-12-30 14:22:15 -0500

    Set a flag.

--- pygame_projects/surface/1_display_surface.py
+++ pygame_projects/surface/1_display_surface.py
@@ -12,4 +12,6 @@ pygame.init()
 WINDOW_WIDTH = 600
 WINDOW_HEIGHT = 300
 display_surface = pygame.display.set_mode([WINDOW_WIDTH, WINDOW_HEIGHT])
+#Creating a flag.
+not_looping = False
 #The main loop for the game
