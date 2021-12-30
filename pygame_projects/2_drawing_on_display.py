510058d0558f3f1bc2e65ae61317b9943bc8ea8f
matthewivezaj73 <81784356+matthewivezaj73@users.noreply.github.com>
2021-12-30 17:25:33 -0500

    Added a comment.

--- pygame_projects/2_drawing_on_display.py
+++ pygame_projects/2_drawing_on_display.py
@@ -40,6 +40,7 @@ pygame.draw.circle(display_surface, YELLOW, (WINDOW_WIDTH//2, WINDOW_HEIGHT//2),
 Rectangle(surface, color, (top-left x, top-left y, width, height))
 """
 pygame.draw.rect(display_surface, CYAN, (500, 0, 100, 100))
+#Adding another rectangle.
 #Setting a flag.
 running = True
 #Starting a while loop
