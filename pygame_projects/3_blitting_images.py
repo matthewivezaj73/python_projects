bebd7ddb89259f3a4791a8bf7eee5d11a7008b68
matthewivezaj73 <81784356+matthewivezaj73@users.noreply.github.com>
2021-12-30 18:19:23 -0500

    Added a surface.

--- pygame_projects/3_blitting_images.py
+++ pygame_projects/3_blitting_images.py
@@ -3,3 +3,6 @@ import pygame
 #Initialize pygame.
 pygame.init()
 #Create a display surface.
+WINDOW_WIDTH = 600
+WINDOW_HEIGHT = 300
+DISPLAY_SURFACE = pygame.display.set_mode((WINDOW_WIDTH, WINDOW_HEIGHT))
