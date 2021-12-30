e1500975edd17f0e06a3b0d2131b074ed77e0157
matthewivezaj73 <81784356+matthewivezaj73@users.noreply.github.com>
2021-12-30 14:38:10 -0500

    Added a comment and set a flag.

--- pygame_projects/2_drawing_on_display.py
+++ pygame_projects/2_drawing_on_display.py
@@ -11,3 +11,6 @@ WINDOW_HEIGHT = 600
 display_surface = pygame.display.set_mode([WINDOW_WIDTH, WINDOW_HEIGHT])
 #Setting a caption.
 pygame.display.set_caption("Drawing objects")
+
+#Setting a flag.
+not_running = False
