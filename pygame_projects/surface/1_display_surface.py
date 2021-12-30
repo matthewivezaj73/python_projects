2f407729e6bc6e62c815e5dc05a2731c11d47d79
matthewivezaj73 <81784356+matthewivezaj73@users.noreply.github.com>
2021-12-30 14:21:23 -0500

    Displaying a window

--- pygame_projects/surface/1_display_surface.py
+++ pygame_projects/surface/1_display_surface.py
@@ -5,5 +5,10 @@ import pygame
 pygame.init()
 
 #Creating the display surface and setting captions.
+
+#Example from pygame.org
+# flags = pygame.OPENGL | pygame.FULLSCREEN
+# window_surface = pygame.display.set_mode((1920, 1080), flags, vsync=1)
 WINDOW_WIDTH = 600
 WINDOW_HEIGHT = 300
+display_surface = pygame.display.set_mode([WINDOW_WIDTH, WINDOW_HEIGHT])
