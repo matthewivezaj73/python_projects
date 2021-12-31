a2f57318c03e8e3491057408ef4cb52f7dde0d2d
matthewivezaj73 <81784356+matthewivezaj73@users.noreply.github.com>
2021-12-31 12:45:32 -0500

    playing sound at a lower volume.

--- pygame_projects/5_adding_sounds.py
+++ pygame_projects/5_adding_sounds.py
@@ -30,7 +30,7 @@ sound_2.play()
 #Delaying the sound by 2 seconds (2000 milliseconds)
 pygame.time.delay(2000)
 #Changing the volume of the outputted sound.
-sound_2.set_volume(.1)
+sound_2.set_volume(.01)
 #Playing the other sound.
 sound_2.play()
 #Setting a flag.
