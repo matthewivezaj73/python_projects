9002ea79e3e3b42186d88f5ed3df2a9b262eef2b
matthewivezaj73 <81784356+matthewivezaj73@users.noreply.github.com>
2021-12-30 20:22:41 -0500

    Added a comment.

--- pygame_projects/4_blitting_text.py
+++ pygame_projects/4_blitting_text.py
@@ -7,4 +7,5 @@ pygame.init()
 #Creating a display surface.
 WINDOW_WIDTH = 600
 WINDOW_HEIGHT = 300
+#Setting the mode of the window.
 display_surface = pygame.display.set_mode(WINDOW_WIDTH, WINDOW_HEIGHT)
