7b2973a2a7c70630eab63e2126ea3dd5dfb5bab6
matthewivezaj73 <81784356+matthewivezaj73@users.noreply.github.com>
2021-12-30 19:03:36 -0500

    Added a comment.

--- pygame_projects/3_blitting_images.py
+++ pygame_projects/3_blitting_images.py
@@ -12,6 +12,7 @@ pygame.display.set_caption("Blitting Images!")
 dragon_left_image = pygame.image.load("dragon_left.png")
 #Creating a rectangle.
 dragon_left_rect = dragon_left_image.get_rect()
+#Positioning the image.
 #The main game loop.
 not_running = False
 #Testing for reaction.
