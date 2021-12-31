2a4e51d73dad5eaf29d680f975f38539aab6a9f3
matthewivezaj73 <81784356+matthewivezaj73@users.noreply.github.com>
2021-12-31 12:37:29 -0500

    changed the name of the wav file.

--- pygame_projects/5_adding_sounds.py
+++ pygame_projects/5_adding_sounds.py
@@ -13,7 +13,7 @@ display_surface = pygame.display.set_mode((WINDOW_WIDTH,WINDOW_HEIGHT))
 pygame.display.set_caption("Adding Sounds!")
 #Loading the sounds.
 sound_1 = pygame.mixer.Sound('Jump.wav')
-sound_2 = pygame.mixer.Sound('Jump.wav')
+sound_2 = pygame.mixer.Sound('powerup.wav')
 
 #Playing the sounds.
 sound_1.play()
