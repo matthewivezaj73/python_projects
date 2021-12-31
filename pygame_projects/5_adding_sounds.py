d66cea4a689ff4ef8216cd2a972674e2750b745a
matthewivezaj73 <81784356+matthewivezaj73@users.noreply.github.com>
2021-12-31 12:24:10 -0500

    Added sound1

--- pygame_projects/5_adding_sounds.py
+++ pygame_projects/5_adding_sounds.py
@@ -12,6 +12,7 @@ display_surface = pygame.display.set_mode((WINDOW_WIDTH,WINDOW_HEIGHT))
 #Creating a caption.
 pygame.display.set_caption("Adding Sounds!")
 #Defining the sounds.
+sound_1 = pygame.micer.Sound('sound_1.wav')
 #Setting a flag.
 not_done = False
 #Testing for the user to quit the game.
