c874a00f10d7db138e0d945752843b84bebbd5b9
matthewivezaj73 <81784356+matthewivezaj73@users.noreply.github.com>
2021-12-31 16:43:06 -0500

    Added a flag.

--- pygame_projects/6_discrete_keyboard_movement.py
+++ pygame_projects/6_discrete_keyboard_movement.py
@@ -11,4 +11,5 @@ display_surface = pygame.display.set_mode((WINDOW_WIDTH, WINDOW_HEIGHT))
 #Adding a caption.
 pygame.display.set_caption("Discrete Movement!")
 #Setting a flag.
+not_running = False
 #The main game loop.
