7c543046618f3c120de9d434faa01a6ae8c2bd72
matthewivezaj73 <81784356+matthewivezaj73@users.noreply.github.com>
2022-01-01 11:05:19 -0500

    Printing the event.

--- pygame_projects/7_mouse_movement.py
+++ pygame_projects/7_mouse_movement.py
@@ -32,6 +32,7 @@ while not not_running:
 
         #Moving based on mouse clicks.
         if event.type == pygame.MOUSEBUTTONDOWN:
+            print(event)
     #Filling the display.
     display_surface.fill((0, 0, 0))
 
