487420504e82617c8a68a860455aa40b1599c9d6
matthewivezaj73 <81784356+matthewivezaj73@users.noreply.github.com>
2022-01-01 09:50:54 -0500

    Added a caption.

--- pygame_projects/7_mouse_movement.py
+++ pygame_projects/7_mouse_movement.py
@@ -9,3 +9,4 @@ WINDOW_HEIGHT = 300
 #Creating a display surface.
 display_surface = pygame.display.set_mode(WINDOW_WIDTH, WINDOW_HEIGHT)
 #Setting a caption.
+pygame.display.set_caption("Mouse Movement!")
