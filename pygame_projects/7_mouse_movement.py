b6110c7e1a1b0c2cb0045d8ca509b7bdc2558a53
matthewivezaj73 <81784356+matthewivezaj73@users.noreply.github.com>
2022-01-01 09:49:14 -0500

    Added a comment.

--- pygame_projects/7_mouse_movement.py
+++ pygame_projects/7_mouse_movement.py
@@ -6,4 +6,5 @@ pygame.init()
 #Creating a game surface.
 WINDOW_WIDTH = 600
 WINDOW_HEIGHT = 300
+#Creating a display surface.
 display_surface = pygame.display.set_mode(WINDOW_WIDTH, WINDOW_HEIGHT)
