740078e4858ba323f555c6731b090ea452faf89f
matthewivezaj73 <81784356+matthewivezaj73@users.noreply.github.com>
2022-01-07 08:47:18 -0500

    Added a basic website scraper i made.

---
+++ basic_web_scraper.py
@@ -0,0 +1,12 @@
+#Importing the library.
+from urllib.request import urlopen
+
+#Creating a variable that points to a site.
+my_site = "http://olympus.realpython.org/profiles/aphrodite"
+#Creating a variable that grabs the text from a site.
+read_site = urlopen(my_site)
+#Creating a variable that reads the content.
+
+#Reading each line on the site
+for line in read_site:
+    print(line)
