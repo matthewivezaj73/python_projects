fb9016f8cbed55c8f635f9d42edcac599c0f1306
matthewivezaj73 <81784356+matthewivezaj73@users.noreply.github.com>
2022-01-08 13:44:04 -0500

    Adding a docstring.

--- data_scraper/classes/scraping_validator.py
+++ data_scraper/classes/scraping_validator.py
@@ -3,4 +3,7 @@ class ScrapingValidator:
     A class that represents checks against 
     the data entered into the scraper.
     """
-    def __init__(self,sitename,wordcount)
+    def __init__(self,sitename,wordcount):
+        """
+        
+        """
