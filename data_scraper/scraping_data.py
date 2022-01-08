50d18f8d9def5f7c8702a340db999ca9204fee0e
matthewivezaj73 <81784356+matthewivezaj73@users.noreply.github.com>
2022-01-08 13:32:30 -0500

    added a while loop.

--- data_scraper/scraping_data.py
+++ data_scraper/scraping_data.py
@@ -3,6 +3,7 @@ from urllib.request import urlopen
 #Setting a flag.
 not_scraped = False
 #Testing for input from the user.
+while not not_scraped:
 #Creating a variable that points to a site.
 my_site = "http://olympus.realpython.org/profiles/aphrodite"
 #Creating a variable that grabs the text from a site.
