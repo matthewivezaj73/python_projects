4f91872917e40589a06dbd12bbc585dc5c786ac8
matthewivezaj73 <81784356+matthewivezaj73@users.noreply.github.com>
2022-01-08 13:31:34 -0500

    Added a comment.

--- data_scraper/scraping_data.py
+++ data_scraper/scraping_data.py
@@ -1,6 +1,6 @@
 #Importing the library.
 from urllib.request import urlopen
-
+#Setting a flag.
 #Creating a variable that points to a site.
 my_site = "http://olympus.realpython.org/profiles/aphrodite"
 #Creating a variable that grabs the text from a site.
