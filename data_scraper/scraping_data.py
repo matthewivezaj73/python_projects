3446b3fa7d96b8b195c279cc09fc9ba5ab9c2a91
matthewivezaj73 <81784356+matthewivezaj73@users.noreply.github.com>
2022-01-08 20:30:51 -0500

    Commented lines out and remove a / in path

--- data_scraper/scraping_data.py
+++ data_scraper/scraping_data.py
@@ -1,7 +1,7 @@
 #Importing the library.
 from urllib.request import urlopen
 #Assinging a variable to the path of the text file.
-my_file = "/text_files/site_text.txt"
+my_file = "text_files/site_text.txt"
 #Importing class.
 from classes.scraping_validator import ScrapingValidator
 #Creating an instance of a class.
@@ -17,17 +17,25 @@ while not not_scraped:
     while not not_site_entered:
         #Asking for input from the user.
         site_entered = input("Please enter the name of a site whose data you would like scaped: ")
-        #Grabbing content from the site.
-        site_entered = my_site_scraper.validateSite(site_entered)
+
         #Validating the site_entered variable.
-        not_site_entered = my_site_scraper.validateSite(site_entered)
+        # not_site_entered = my_site_scraper.validateSite(site_entered)
     #Opening a file.
-#Creating a variable that points to a site.
-my_site = "http://olympus.realpython.org/profiles/aphrodite"
-#Creating a variable that grabs the text from a site.
-read_site = urlopen(my_site)
-#Opening the text file to work with.
-with open(my_file) as fold:
+        #Creating a variable that points to a site.
+        # my_site = "http://olympus.realpython.org/profiles/aphrodite"
+        #Creating a variable that grabs the text from a site.
+        read_site = urlopen(site_entered)
+        for line in read_site:
+            print(line)
+        #Opening the text file to work with.
+    #     with open(my_file) as fold:
+
+    #         fold.write(read_site)
+    # # with open("text_files/site_text.csv","w+") as fawn:
+    #     #Going through each line in the list.
+    #     for line in fold:
+    #         #Writing each line to the file.
+    #         fawn.write(line)             
 
 
 
