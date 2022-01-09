46a6cd31f858936b0f538e814949f30e37927fe0
matthewivezaj73 <81784356+matthewivezaj73@users.noreply.github.com>
2022-01-08 20:39:41 -0500

    Asking the user what they would like to do.

--- data_scraper/scraping_data.py
+++ data_scraper/scraping_data.py
@@ -11,6 +11,7 @@ not_scraped = False
 #Testing for input from the user.
 while not not_scraped:
     #Asking the user what they want to do.
+    user_choice = input("Please enter \'r\' if you would like to read the contents of a website.")
     #Printing a message to the user.
     #Setting a flag.
     not_site_entered = False
