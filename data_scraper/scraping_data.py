4de79b7af8f6f6424a6687ad26b110325362f047
matthewivezaj73 <81784356+matthewivezaj73@users.noreply.github.com>
2022-01-08 21:39:09 -0500

    added except and switching loc of try.

--- data_scraper/scraping_data.py
+++ data_scraper/scraping_data.py
@@ -15,30 +15,32 @@ while not not_scraped:
     user_choice = input("Please enter \'r\' if you would like to read the contents of a website.\n\nPlease enter \'csv\' if you would like to read the contents of the csv file.\n\nPlease enter \'rem\' if you would like to remove characters from the csv.")
     #Handling the case where the user enters r.
     if user_choice.lower() == "r":
-        #Adding a try block in case there is an error with the html request.
-        #Setting a flag.
-        not_site_entered = False
-        #Testing to ensure the data is ok.
-        while not not_site_entered:
-            #Asking for input from the user.
-            site_entered = input("Please enter the name of a site whose data you would like scraped or \'q\' to quit: ")
-            #Creating a variable that grabs the text from a site.
-            read_site = urlopen(site_entered)
-            #Creating a blank list.
-            my_list = []
-            for line in read_site:
-                #Appending each line along with "," to the list.
-                my_list.append(str(line)+",")             
-            #Opening the text file to work with.
-            with open(my_file) as fold:
 
-                with open("text_files/site_text.csv","w+") as fawn:
-                    # Going through each line in the list.
-                    for line in my_list:
-                        #Writing each line to the file.
-                        fawn.write(str(line))
-            #Setting flag to true.
-            not_site_entered = True
+            #Setting a flag.
+            not_site_entered = False
+            #Testing to ensure the data is ok.
+            while not not_site_entered:
+                #Asking for input from the user.
+                site_entered = input("Please enter the name of a site whose data you would like scraped or \'q\' to quit: ")
+                #Creating a variable that grabs the text from a site.
+                read_site = urlopen(site_entered)
+                #Creating a blank list.
+                my_list = []
+                for line in read_site:
+                    #Appending each line along with "," to the list.
+                    my_list.append(str(line)+",")             
+                #Opening the text file to work with.
+                with open(my_file) as fold:
+
+                    with open("text_files/site_text.csv","w+") as fawn:
+                        # Going through each line in the list.
+                        for line in my_list:
+                            #Writing each line to the file.
+                            fawn.write(str(line))
+                #Setting flag to true.
+                not_site_entered = True
+            except:
+                print("Sorry, but we can't do that right now")
     #Handling the case where the user wants to read the contents of the csv file.
     elif user_choice.lower() == "csv":
         with open("text_files/site_text.csv") as fawn:
