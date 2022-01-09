7b0abb6e2ecc37771b6917dd060e6defa46019c0
matthewivezaj73 <81784356+matthewivezaj73@users.noreply.github.com>
2022-01-08 21:13:21 -0500

    Added a comment and way to read line in file.

--- data_scraper/scraping_data.py
+++ data_scraper/scraping_data.py
@@ -45,5 +45,6 @@ while not not_scraped:
                 print(line)
     #Handling the case where the user wants to remove a character from the csv file.
     elif user_choice.lower() == "rem":
+        #Opening the file for reading.
         with open("text_files/site_text.csv","r") as fawn:
-
+            read_lines = fawn.readlines()
