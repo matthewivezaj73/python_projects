54c78d4aef639a8fb173dad77bbfb13a07e92715
matthewivezaj73 <81784356+matthewivezaj73@users.noreply.github.com>
2022-01-08 20:52:10 -0500

    edit text and indented with open blocks.

--- data_scraper/scraping_data.py
+++ data_scraper/scraping_data.py
@@ -13,32 +13,33 @@ while not not_scraped:
     #Asking the user what they want to do.
     user_choice = input("Please enter \'r\' if you would like to read the contents of a website.")
     #Handling the case where the user enters r.
-    #Printing a message to the user.
-    #Setting a flag.
-    not_site_entered = False
-    #Testing to ensure the data is ok.
-    while not not_site_entered:
-        #Asking for input from the user.
-        site_entered = input("Please enter the name of a site whose data you would like scaped: ")
+    if user_choice.lower() == "r":
 
-        #Validating the site_entered variable.
-        # not_site_entered = my_site_scraper.validateSite(site_entered)
-    #Opening a file.
-        #Creating a variable that points to a site.
-        # my_site = "http://olympus.realpython.org/profiles/aphrodite"
-        #Creating a variable that grabs the text from a site.
-        read_site = urlopen(site_entered)
-        for line in read_site:
-            print(line)
-    #Opening the text file to work with.
-    with open(my_file) as fold:
+        #Printing a message to the user.
+        #Setting a flag.
+        not_site_entered = False
+        #Testing to ensure the data is ok.
+        while not not_site_entered:
+            #Asking for input from the user.
+            site_entered = input("Please enter the name of a site whose data you would like scraped: ")
 
-        fold.write(read_site)
-        with open("text_files/site_text.csv","w+") as fawn:
-            # Going through each line in the list.
-            for line in fold:
-                #Writing each line to the file.
-                fawn.write(line)             
+            #Validating the site_entered variable.
+            # not_site_entered = my_site_scraper.validateSite(site_entered)
+            #Opening a file.
+            #Creating a variable that points to a site.
+            # my_site = "http://olympus.realpython.org/profiles/aphrodite"
+            #Creating a variable that grabs the text from a site.
+            read_site = urlopen(site_entered)
+            for line in read_site:
+                print(line)
+            #Opening the text file to work with.
+            with open(my_file) as fold:
+
+                with open("text_files/site_text.csv","w+") as fawn:
+                    # Going through each line in the list.
+                    for line in fold:
+                        #Writing each line to the file.
+                        fawn.write(line)             
 
 
 
