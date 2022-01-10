63abcfd8988a9024654ef2567ac39015e31b739e
matthewivezaj73 <81784356+matthewivezaj73@users.noreply.github.com>
2022-01-10 07:22:37 -0500

    Modified the parameter in urlopen.

--- data_scraping/parsing/parsing_main.py
+++ data_scraping/parsing/parsing_main.py
@@ -10,7 +10,7 @@ while not not_done:
     #Asking for a link from the user.
     my_link = input("Please enter a link whose page you would like to scrape in proper form (i.e. http, https, www., .com, .net): ")
     # print(bs.h1)
-    response = req.urlopen(html)
+    response = req.urlopen(my_link)
 #Printing the response returned.
 # print(type(response))
 
