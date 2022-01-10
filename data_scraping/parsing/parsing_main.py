f66377a2f8e2d9a0fc4d344a1ce5f598df281924
matthewivezaj73 <81784356+matthewivezaj73@users.noreply.github.com>
2022-01-10 07:13:30 -0500

    importing lib

--- data_scraping/parsing/parsing_main.py
+++ data_scraping/parsing/parsing_main.py
@@ -1,5 +1,6 @@
 #importing libraries.
-from urllib.request import urlopen
-from bs4 import BeautifulSoup
+import urllib.request as req
+# from bs4 import BeautifulSoup
 #Creating a variable and assigning it a web page.
-html = urlopen('http://www.pythonscraping.com/pages/page1.html')
+html = 'https://en.wikipedia.org/wiki/List_of_most_popular_websites'
+# print(bs.h1)
