8c0b5de4f753de2f84e4d0510ba53e59c1ca5504
matthewivezaj73 <81784356+matthewivezaj73@users.noreply.github.com>
2022-01-10 07:18:34 -0500

    commented link out.

--- data_scraping/parsing/parsing_main.py
+++ data_scraping/parsing/parsing_main.py
@@ -2,7 +2,8 @@
 import urllib.request as req
 # from bs4 import BeautifulSoup
 #Creating a variable and assigning it a web page.
-html = 'https://accounts.google.com/ServiceLogin/signinchooser?service=mail&passive=1209600&osid=1&continue=https%3A%2F%2Fmail.google.com%2Fmail%2Fu%2F0%2F&followup=https%3A%2F%2Fmail.google.com%2Fmail%2Fu%2F0%2F&emr=1&flowName=GlifWebSignIn&flowEntry=ServiceLogin#inbox'
+# html = 'https://accounts.google.com/ServiceLogin/signinchooser?service=mail&passive=1209600&osid=1&continue=https%3A%2F%2Fmail.google.com%2Fmail%2Fu%2F0%2F&followup=https%3A%2F%2Fmail.google.com%2Fmail%2Fu%2F0%2F&emr=1&flowName=GlifWebSignIn&flowEntry=ServiceLogin#inbox'
+
 # print(bs.h1)
 response = req.urlopen(html)
 #Printing the response returned.
