f3b51e345898f4b42e9c1de57e1c0662f6d6c6b5
matthewivezaj73 <81784356+matthewivezaj73@users.noreply.github.com>
2022-01-10 07:21:33 -0500

    added a flag.

--- data_scraping/parsing/parsing_main.py
+++ data_scraping/parsing/parsing_main.py
@@ -4,6 +4,7 @@ import urllib.request as req
 #Creating a variable and assigning it a web page.
 # html = 'https://accounts.google.com/ServiceLogin/signinchooser?service=mail&passive=1209600&osid=1&continue=https%3A%2F%2Fmail.google.com%2Fmail%2Fu%2F0%2F&followup=https%3A%2F%2Fmail.google.com%2Fmail%2Fu%2F0%2F&emr=1&flowName=GlifWebSignIn&flowEntry=ServiceLogin#inbox'
 #Adding a flag.
+not_done = False
 #Testing for a reponse from the user.
 my_link = input("Please enter a link whose page you would like to scrape in proper form (i.e. http, https, www., .com, .net): ")
 # print(bs.h1)
