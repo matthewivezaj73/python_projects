5a593a5fb173319fad32c391ac2a29a958c8fbd2
matthewivezaj73 <81784356+matthewivezaj73@users.noreply.github.com>
2022-01-10 07:14:12 -0500

    Assigned the urlopen request to a variable.

--- data_scraping/parsing/parsing_main.py
+++ data_scraping/parsing/parsing_main.py
@@ -4,3 +4,4 @@ import urllib.request as req
 #Creating a variable and assigning it a web page.
 html = 'https://en.wikipedia.org/wiki/List_of_most_popular_websites'
 # print(bs.h1)
+response = req.urlopen(html)
