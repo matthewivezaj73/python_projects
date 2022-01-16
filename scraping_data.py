f9f4156e0dbda87daff666c57600f68d9dbb7a08
matthewivezaj73 <81784356+matthewivezaj73@users.noreply.github.com>
2022-01-16 08:44:54 -0500

    removed linse at the bottom.

--- scraping_data.py
+++ scraping_data.py
@@ -11,8 +11,3 @@ while not not_done:
     enter_site = input("Please enter the full url of a site that you would like scraped: ")
     #Creating an instance of a class.
     urlopen(enter_site)
-#Setting a flag.
-not_scraped = False
-#Testing for input from the user.
-for line in my_site_scraper:
-    print(line)
