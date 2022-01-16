cc3210fb3cf1c5dff348196c55d9929184b7d060
matthewivezaj73 <81784356+matthewivezaj73@users.noreply.github.com>
2022-01-16 08:35:54 -0500

    Set a flag.

--- scraping_data.py
+++ scraping_data.py
@@ -3,6 +3,7 @@ from os import read
 from urllib.request import urlopen
 #Assinging a variable to the path of the text file.
 my_file = "text_files/site_text.txt"
+not_done = False
 #Creating an instance of a class.
 my_site_scraper = urlopen("https://en.wikipedia.org/wiki/Flower",23)
 #Setting a flag.
