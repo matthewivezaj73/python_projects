f9a311ab721c487443445572a2566815cc766f6d
matthewivezaj73 <81784356+matthewivezaj73@users.noreply.github.com>
2022-01-16 13:56:48 -0500

    removed the for loop.

--- collecting_covid_data.py
+++ collecting_covid_data.py
@@ -3,3 +3,7 @@ from urllib.request import urlopen
 
 #assigning a site to a variable.
 oakland_county = "https://www.oakgov.com/advantageoakland/resources/Documents/OaklandCounty_EconomicOutlookSummary_2021-2023.pdf"
+
+#Reading each line on the site
+
+    print(line)
