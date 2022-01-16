27d230d261329fd292d9a22e3316592ddaf5b2fb
matthewivezaj73 <81784356+matthewivezaj73@users.noreply.github.com>
2022-01-15 23:13:22 -0500

    changed the function call.

--- scraping_data.py
+++ scraping_data.py
@@ -3,10 +3,8 @@ from os import read
 from urllib.request import urlopen
 #Assinging a variable to the path of the text file.
 my_file = "text_files/site_text.txt"
-#Importing class.
-from classes.scraping_validator import ScrapingValidator
 #Creating an instance of a class.
-my_site_scraper = ScrapingValidator("https://en.wikipedia.org/wiki/Flower",23)
+my_site_scraper = urlopen("https://en.wikipedia.org/wiki/Flower",23)
 #Setting a flag.
 not_scraped = False
 #Testing for input from the user.
