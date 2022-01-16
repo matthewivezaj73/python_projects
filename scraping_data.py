a3eda3d0019ad48aac801baf03c29eae8d4bfdca
matthewivezaj73 <81784356+matthewivezaj73@users.noreply.github.com>
2022-01-15 23:09:43 -0500

    Reused some code.

---
+++ scraping_data.py
@@ -0,0 +1,12 @@
+#Importing the library.
+from os import read
+from urllib.request import urlopen
+#Assinging a variable to the path of the text file.
+my_file = "text_files/site_text.txt"
+#Importing class.
+from classes.scraping_validator import ScrapingValidator
+#Creating an instance of a class.
+my_site_scraper = ScrapingValidator("https://www.google.com/",23)
+#Setting a flag.
+not_scraped = False
+#Testing for input from the user.
