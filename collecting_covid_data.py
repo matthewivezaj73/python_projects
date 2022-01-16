4a8ecfe73895fb6ef97bd843fc20ebde7ef249b9
matthewivezaj73 <81784356+matthewivezaj73@users.noreply.github.com>
2022-01-16 13:58:28 -0500

    opening the site.

--- collecting_covid_data.py
+++ collecting_covid_data.py
@@ -3,7 +3,8 @@ from urllib.request import urlopen
 
 #assigning a site to a variable.
 oakland_county = "https://www.oakgov.com/advantageoakland/resources/Documents/OaklandCounty_EconomicOutlookSummary_2021-2023.pdf"
-
+#Opening the site.
+read_url = urlopen(oakland_county)
 #Reading each line on the site
 
-print(oakland_county)
+print()
