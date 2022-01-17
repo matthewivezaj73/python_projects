badacc763dfa52b05d713a1bb37e7f0ca211b1d9
matthewivezaj73 <81784356+matthewivezaj73@users.noreply.github.com>
2022-01-17 12:08:28 -0500

    Added a docstring.

--- system_commands.py
+++ system_commands.py
@@ -6,3 +6,10 @@ import subprocess
 # We can also be clever and only output what we are interested in, 
 # instead of sifting through unessential information.  Don’t work harder, work smarter.)
 output = subprocess.Popen(['netstat','-ano'],stdout=subprocess.PIPE).commmunicate()
+"""
+i.	output – variable we are instantiating.  
+ii.	subprocess – calls the subprocess module
+iii.	Popen – calls a subroutine defined in subprocess
+iv.	[‘netstat’, ‘-ano’] – We are feeding the command netstat as text into Popen with the arguments ‘-ano’.  If we only wanted to send netstat by itself as default, we would have ‘netstat’ with no brackets.  
+v.	stdout=subprocess.PIPE – We are sending the output back to this call for connection to the variable. 
+"""
