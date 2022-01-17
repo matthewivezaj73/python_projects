ec08bc42b500a2cd12dc510438891ef5815b42e5
matthewivezaj73 <81784356+matthewivezaj73@users.noreply.github.com>
2022-01-17 15:03:15 -0500

    File that grabs netstat output.

---
+++ cmd_output/system_commands.py
@@ -0,0 +1,54 @@
+#Importing libraries.
+import os
+import subprocess
+#Subprocess allow us to send command line commands to windows through python.
+#(We do this so that we can piggyback multiple commands.  It allows us to save time typing.  
+# We can also be clever and only output what we are interested in, 
+# instead of sifting through unessential information.  Don’t work harder, work smarter.)
+output = subprocess.Popen(['netstat','-ano'],stdout=subprocess.PIPE).communicate()
+"""
+i.	output – variable we are instantiating.  
+ii.	subprocess – calls the subprocess module
+iii.	Popen – calls a subroutine defined in subprocess
+iv.	[‘netstat’, ‘-ano’] – We are feeding the command netstat as text into Popen with the arguments ‘-ano’.  If we only wanted to send netstat by itself as default, we would have ‘netstat’ with no brackets.  
+v.	stdout=subprocess.PIPE – We are sending the output back to this call for connection to the variable. 
+"""
+# #Printing output 
+# print(output)
+#Checking the type of the
+type(output)
+#Setting new line breaks so that the data comes out all clean.
+new_lines = output[0].split('\r\n  '.encode())
+
+#Opening the parent text file.
+with open("text_files/my_output.txt") as text_file:
+    #Checking if the file already exists.
+    if os.path.isfile("text_files/my_CSV_output.csv"):
+        #Asking the user if they would like to overwrite the file.
+        my_question = input("the file already exists, would you like to overwrite it?\nPlease respond with Y/N for yes(Y) and no(N) respectively: ")
+        #Creating a child csv file.
+        with open("text_files/my_CSV_output.csv","w") as Exists_csv_output:
+            #Creating a for loop.
+            for line in new_lines:
+                #Checking if the line is equal to establised
+                if line.find("ESTABLISHED".encode()):
+                    print(line)
+                    line = str(line).encode()
+                    Exists_csv_output.write(str(line))
+    #Handling the case where the file does not exist in the text_files directory.
+    elif not os.path.isfile("text_files/my_CSV_output.csv"):
+
+        #Creating a child csv file.
+        with open("text_files/my_CSV_output.csv","w+") as csv_output:
+            read_output = csv_output.readlines()
+            csv_output.write("LISTENING")
+            #print(len(read_output))
+            #Creating a for loop.
+            for line in new_lines:#
+                #Checking if the line is equal to establised
+                if line.find("LISTENING".encode()):
+                    print(line)
+                    my_line = "LISTENING".encode()
+                    csv_output.write(str(line))
+            csv_output.write("\n")
+                
