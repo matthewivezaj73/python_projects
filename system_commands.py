a36926ddde9c65d1a8f194f33cd7d5eb34b3cfdb
matthewivezaj73 <81784356+matthewivezaj73@users.noreply.github.com>
2022-01-17 12:04:57 -0500

    Added a comment.

--- system_commands.py
+++ system_commands.py
@@ -2,3 +2,6 @@
 import os
 import subprocess
 #Subprocess allow us to send command line commands to windows through python.
+#(We do this so that we can piggyback multiple commands.  It allows us to save time typing.  
+# We can also be clever and only output what we are interested in, 
+# instead of sifting through unessential information.  Don’t work harder, work smarter.)
