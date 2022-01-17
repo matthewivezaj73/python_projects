dd3c4ee93c7889c973d9b4beb1cdc12f400074a0
matthewivezaj73 <81784356+matthewivezaj73@users.noreply.github.com>
2022-01-16 21:58:11 -0500

    added a new file.

--- covid_vaccine_tracker.py
+++ covid_vaccine_tracker.py
@@ -1,1 +1,12 @@
-https://data.thetimesherald.com/covid-19-vaccine-tracker/michigan/wayne-county/26163/
+#Importing the library.
+from urllib.request import urlopen
+
+#Creating a variable that points to a site.
+my_site = "https://data.thetimesherald.com/covid-19-vaccine-tracker/michigan/wayne-county/26163/"
+#Creating a variable that grabs the text from a site.
+read_site = urlopen(my_site)
+#Creating a variable that reads the content.
+
+#Reading each line on the site
+for line in read_site:
+    print(line)
