ceffb0bce4ed61437fb670294471b19c967fb59e
matthewivezaj73 <81784356+matthewivezaj73@users.noreply.github.com>
2022-01-17 21:23:17 -0500

    Added a line to grab the time.

--- cmd_output/system_commands.py
+++ cmd_output/system_commands.py
@@ -8,6 +8,7 @@ from datetime import date
 # We can also be clever and only output what we are interested in, 
 # instead of sifting through unessential information.  Don’t work harder, work smarter.)
 output = subprocess.Popen(['netstat','-ano'],stdout=subprocess.PIPE).communicate()
+now=datetime.datetime.now()
 """
 i.	output – variable we are instantiating.  
 ii.	subprocess – calls the subprocess module
