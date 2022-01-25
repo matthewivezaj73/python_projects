9fe1e0f30557d5294c6e50c32c78b30e1dbc404b
matthewivezaj73 <81784356+matthewivezaj73@users.noreply.github.com>
2022-01-25 07:56:38 -0500

    cleaned up the link

--- site_cleaner/site_cleaner_main.py
+++ site_cleaner/site_cleaner_main.py
@@ -4,9 +4,8 @@ from urllib.request import urlopen
 #Assinging a variable to the path of the text file.
 my_file = "text_files/site_text.txt"
 #Importing class.
-from classes.scraping_validator import ScrapingValidator
 #Creating an instance of a class.
-my_site_scraper = ScrapingValidator("https://www.google.com/",23)
+my_site_scraper = "https://www.google.com/"
 #Reading the conents of the site.
 read_site = urlopen(my_site_scraper)
 #Setting a flag.
