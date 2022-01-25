bfa9afe5eb30baebcd3c51d2ac98173daa27c2ed
matthewivezaj73 <81784356+matthewivezaj73@users.noreply.github.com>
2022-01-25 07:56:20 -0500

    added a function to read the site input

--- site_cleaner/site_cleaner_main.py
+++ site_cleaner/site_cleaner_main.py
@@ -7,5 +7,7 @@ my_file = "text_files/site_text.txt"
 from classes.scraping_validator import ScrapingValidator
 #Creating an instance of a class.
 my_site_scraper = ScrapingValidator("https://www.google.com/",23)
+#Reading the conents of the site.
+read_site = urlopen(my_site_scraper)
 #Setting a flag.
 
