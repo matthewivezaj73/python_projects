b024406fb02796933842090fe5eb68f48e0f1aef
matthewivezaj73 <81784356+matthewivezaj73@users.noreply.github.com>
2022-02-20 18:31:17 -0500

    Added a comment.

--- host_info/host_main.py
+++ host_info/host_main.py
@@ -1,6 +1,7 @@
 #Import libraries
 import socket
 import psutil
+#Creating a blank list.
 #Creating a flag.
 not_done = False
 #Testing for user input.
@@ -9,4 +10,3 @@ while not not_done:
     user_choice = input("Please enter a if you would like to add a configuration.")
     #Handling the case where the user enters a.
     if user_choice.lower() == "a":
-        
