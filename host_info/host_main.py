262b6967279b4fd75418928bb6ddd7a03ef29e23
matthewivezaj73 <81784356+matthewivezaj73@users.noreply.github.com>
2022-02-20 18:30:49 -0500

    Handling a case.

--- host_info/host_main.py
+++ host_info/host_main.py
@@ -8,4 +8,5 @@ while not not_done:
     #Asking the user for a choice.
     user_choice = input("Please enter a if you would like to add a configuration.")
     #Handling the case where the user enters a.
-    
+    if user_choice.lower() == "a":
+        
