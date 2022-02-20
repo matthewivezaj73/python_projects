a6b10b7502a9f8c41d9c00a6bcca99dc88b76513
matthewivezaj73 <81784356+matthewivezaj73@users.noreply.github.com>
2022-02-20 18:30:08 -0500

    Added a comment.

--- host_info/host_main.py
+++ host_info/host_main.py
@@ -5,4 +5,5 @@ import psutil
 not_done = False
 #Testing for user input.
 while not not_done:
+    #Asking the user for a choice.
     user_choice = input("Please enter a if you would like to add a configuration.")
