dde57c56c854e3fda4195858ac60ca93e0b1e1b6
matthewivezaj73 <81784356+matthewivezaj73@users.noreply.github.com>
2022-02-20 20:50:50 -0500

    commit

--- hashing/hashing_main.py
+++ hashing/hashing_main.py
@@ -4,4 +4,5 @@ import hashlib
 not_done = False
 #Testing for user input.
 while not not_done:
+    #Asking the user for iput.
     input_text = input("Please enter a phrase you would like hashed: ")
