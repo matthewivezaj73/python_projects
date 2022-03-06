4d418fe4e60a26ccc2cd3c69aac2e9640b40cdc4
matthewivezaj73 <81784356+matthewivezaj73@users.noreply.github.com>
2022-03-06 09:07:23 -0500

    Added a comment.

--- hashing text/hashing_text_main.py
+++ hashing text/hashing_text_main.py
@@ -4,4 +4,5 @@ from classes.hash_me import *
 not_done = False
 #Testing for the users input.
 while not not_done:
+    #Asking the user what they would like to do.
     user_input = input("Enter \'S\' \n\n**(without single quotes)** \n\nif you would like to add a string to be hashed.")
