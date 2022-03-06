1b88e508bfe27f99344880c895d743360d652e50
matthewivezaj73 <81784356+matthewivezaj73@users.noreply.github.com>
2022-03-06 09:01:03 -0500

    Added a docstring.

--- hashing text/classes/hash_me.py
+++ hashing text/classes/hash_me.py
@@ -3,3 +3,6 @@ class Hashing:
     A class that represents the act of hashing text.
     """
     def __init__(self,hashed_input):
+        """
+        Defining the hashed_input attribute.
+        """
