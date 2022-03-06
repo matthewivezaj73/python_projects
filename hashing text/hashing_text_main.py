bae63b6d1c38d0830959a81e95a54ed54eb7bac3
matthewivezaj73 <81784356+matthewivezaj73@users.noreply.github.com>
2022-03-06 09:06:57 -0500

    added some text.

--- hashing text/hashing_text_main.py
+++ hashing text/hashing_text_main.py
@@ -3,3 +3,5 @@ from classes.hash_me import *
 #Creating a flag.
 not_done = False
 #Testing for the users input.
+while not not_done:
+    user_input = input("Enter \'S\' \n\n**(without single quotes)** \n\nif you would like to add a sring to be hashed.")
