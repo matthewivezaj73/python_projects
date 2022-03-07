a8aad4726a8018c1784e689875fbfae63562c95e
matthewivezaj73 <81784356+matthewivezaj73@users.noreply.github.com>
2022-03-07 08:47:04 -0500

    added a comment and checking on local host.

--- check_ports.py
+++ check_ports.py
@@ -2,3 +2,6 @@
 import socket
 # Begin checking for all open ports.
 open_ports = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+#Checking on the local host.
+result = open_ports.connect_ex(('127.0.0.1', 3306))
+
