a9449fb0fcd4bf6b0c539c2564b4e10087f27fcc
matthewivezaj73 <81784356+matthewivezaj73@users.noreply.github.com>
2022-03-09 20:14:59 -0500

    added a case.

--- Hash_message/hash_main.py
+++ Hash_message/hash_main.py
@@ -4,3 +4,5 @@ not_done = False
 while not not_done:
     #Asking the user what they would like to do.
     user_choice = input("Please enter the type of encryption you would like to encrypt your message in: ")
+    if user_choice.lower() == "sha256":
+        
