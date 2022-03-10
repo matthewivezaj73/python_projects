17b8774a4b1d740088dfa369a5160b64670fa830
matthewivezaj73 <81784356+matthewivezaj73@users.noreply.github.com>
2022-03-09 20:16:48 -0500

    Added a comment.

--- Hash_message/hash_main.py
+++ Hash_message/hash_main.py
@@ -9,4 +9,5 @@ while not not_done:
     user_choice = input("Please enter the type of encryption you would like to encrypt your message in: ")
     #Handling the case where the user enters sha256.
     if user_choice.lower() == "sha256":
+        #Assigning the encrypted message to a variable.
         your_choice = sha256(user_choice)
