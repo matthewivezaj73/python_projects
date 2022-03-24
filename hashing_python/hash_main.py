3f9dd400dbd2fe7d103283106a2cedfc4ba5ce3b
matthewivezaj73 <81784356+matthewivezaj73@users.noreply.github.com>
2022-03-23 20:58:13 -0400

    encoded the hashed value.

--- hashing_python/hash_main.py
+++ hashing_python/hash_main.py
@@ -4,8 +4,8 @@ import hashlib
 #Asking the individual for their name.
 your_name = input("Please enter your name here: ")
 #Assigning a sha256 hash to a variable.
-hashed_value = hashlib.sha256(your_name)
+hashed_value = hashlib.sha256(your_name.encode())
 # creating a generic constructor
 generic = hashlib.new('sha256')
 print("Here is your name in a hashed value!")
-print(named)
+print(hashed_value)
