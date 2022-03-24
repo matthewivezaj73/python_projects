c8987ff8bcc57addabdc1359ff4f11d5addc2c82
matthewivezaj73 <81784356+matthewivezaj73@users.noreply.github.com>
2022-03-23 21:11:28 -0400

    indented lines.

--- hashing_python/hash_main.py
+++ hashing_python/hash_main.py
@@ -8,11 +8,10 @@ while not not_done:
     user_choice = input("Please enter the type of encoding you would like to encode your string in: ")
     #Adding a case.
     if user_choice.lower() == "sha256":
-
-    #Asking the individual for their name.
-    your_name = input("Please enter your name here: ")
-    #Assigning a sha256 hash to a variable.
-    hashed_value = hashlib.sha256(your_name.encode())
-    # creating a generic constructor
-    generic = hashlib.new('sha256')
-    print(f"Here is your name in a hashed value: {hashed_value}")
+        #Asking the individual for their name.
+        your_name = input("Please enter your text here: ")
+        #Assigning a sha256 hash to a variable.
+        hashed_value = hashlib.sha256(your_name.encode())
+        # creating a generic constructor
+        generic = hashlib.new('sha256')
+        print(f"Here is your name in a hashed value: {hashed_value}")
