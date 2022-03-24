6b128e8668fed11afae13c4a217f06db67953bd6
matthewivezaj73 <81784356+matthewivezaj73@users.noreply.github.com>
2022-03-24 13:05:58 -0400

    added a method.

--- hashing_python/classes/hashes.py
+++ hashing_python/classes/hashes.py
@@ -2,3 +2,13 @@ class hashes:
     """ 
     Creating a class that will represent the action of applying a hash to a string.
     """
+    def encrypt(self,algorithm):
+    
+        #Asking the individual for their name.
+        your_phrase = input(b"Please enter your text here: ")
+        #Assigning a sha256 hash to a variable.
+        hashed_value = hashlib.sha1(your_phrase.encode())
+        # creating a generic constructor
+        generic = hashlib.new('sha1')
+        #Printing the hashed value.
+        print(f"Here is your name in a hashed value: {hashed_value}")
