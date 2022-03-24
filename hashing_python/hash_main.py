218f33702c17e1835a1186cfbf214c180023fcc2
matthewivezaj73 <81784356+matthewivezaj73@users.noreply.github.com>
2022-03-23 21:09:07 -0400

    added a while loop to begin testing.

--- hashing_python/hash_main.py
+++ hashing_python/hash_main.py
@@ -3,10 +3,11 @@ import hashlib
 #Created a flag.
 not_done = False
 #Testing for input.
-#Asking the individual for their name.
-your_name = input("Please enter your name here: ")
-#Assigning a sha256 hash to a variable.
-hashed_value = hashlib.sha256(your_name.encode())
-# creating a generic constructor
-generic = hashlib.new('sha256')
-print(f"Here is your name in a hashed value: {hashed_value}")
+while not not_done:
+    #Asking the individual for their name.
+    your_name = input("Please enter your name here: ")
+    #Assigning a sha256 hash to a variable.
+    hashed_value = hashlib.sha256(your_name.encode())
+    # creating a generic constructor
+    generic = hashlib.new('sha256')
+    print(f"Here is your name in a hashed value: {hashed_value}")
