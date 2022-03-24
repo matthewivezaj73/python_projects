d8e41b35015a20ef7d59e601adf9fe4f6acb4025
matthewivezaj73 <81784356+matthewivezaj73@users.noreply.github.com>
2022-03-23 20:56:24 -0400

    asking the user for their name.

--- hashing_python/hash_main.py
+++ hashing_python/hash_main.py
@@ -2,7 +2,7 @@
 import hashlib
 
 #Asking the individual for their name.
-
+your_name = input("Please enter your name here: ")
 #Assigning a sha256 hash to a variable.
 named = hashlib.sha256()
 # creating a generic constructor
