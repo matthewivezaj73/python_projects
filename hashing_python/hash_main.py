496059e2116ec00aa6fb85b2394b06addcd4b3f9
matthewivezaj73 <81784356+matthewivezaj73@users.noreply.github.com>
2022-03-23 21:09:52 -0400

    grabbing user input.

--- hashing_python/hash_main.py
+++ hashing_python/hash_main.py
@@ -4,6 +4,7 @@ import hashlib
 not_done = False
 #Testing for input.
 while not not_done:
+    user_choice = input("Please enter the type of encoding you would like to encode your string in: ")
     #Asking the individual for their name.
     your_name = input("Please enter your name here: ")
     #Assigning a sha256 hash to a variable.
