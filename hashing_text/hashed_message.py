65b61b0e202e2451735f567bc862851a9587d945
matthewivezaj73 <81784356+matthewivezaj73@users.noreply.github.com>
2022-03-27 17:47:06 -0400

    Added a comment.

--- hashing_text/hashed_message.py
+++ hashing_text/hashed_message.py
@@ -2,10 +2,10 @@
 from classes.hashing_methods import Hashing
 #Setting a flag.
 not_done = False
+#Instantiating the class.
 #Testing for user input.
 while not not_done:
     #Requesting user input.
     user_choice = input("Enter an algorithm to generate a string and encrypt it in that algorithm: ")
     #Handling the case where the user selects SHA256.
     if user_choice.lower() == "sha256":
-        
