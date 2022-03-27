4b471adff666a98d992bc81c71e4fcbb2692bea2
matthewivezaj73 <81784356+matthewivezaj73@users.noreply.github.com>
2022-03-27 17:30:13 -0400

    Added more attributes.

--- hashing_text/classes/hashing_methods.py
+++ hashing_text/classes/hashing_methods.py
@@ -3,7 +3,7 @@ class Hashing:
     A class that acts as a virtual representation 
     of a compilation of different hashing methods.
     """
-    def __init__(self,hashed_message, encryption_type):
+    def __init__(self,hashed_message, encryption_type, byte_size):
         """
         Initializing the following attributes:
             - hashed_message
