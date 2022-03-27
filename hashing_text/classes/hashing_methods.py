00b50f6b9fc62be031b65b8426efa493573f5cb6
matthewivezaj73 <81784356+matthewivezaj73@users.noreply.github.com>
2022-03-27 17:28:21 -0400

    Created init function.

--- hashing_text/classes/hashing_methods.py
+++ hashing_text/classes/hashing_methods.py
@@ -3,3 +3,5 @@ class Hashing:
     A class that acts as a virtual representation 
     of a compilation of different hashing methods.
     """
+    def __init__(self,hashed_message):
+        self.hashed_message = hashed_message
