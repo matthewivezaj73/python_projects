2ef6571983a697127b6270ae81e0aea12ac2f0ea
matthewivezaj73 <81784356+matthewivezaj73@users.noreply.github.com>
2022-03-28 11:04:00 -0400

    added comment and changed var name.

--- numpy02.py
+++ numpy02.py
@@ -1,6 +1,7 @@
 #Importing library.
 from numpy.random import randn
 #Creating a dictionaru.
-data = {my_var : radn() for my_var in range(7)}
+data = {my_var : randn() for my_var in range(7)}
+#A for loop to access each line.
 for line in data:
     print(line)
