95fbcf90285942ea048bde93704b583722dcc4b6
matthewivezaj73 <81784356+matthewivezaj73@users.noreply.github.com>
2022-04-23 19:21:20 -0400

    Cut the line

--- networking/initiate_network.py
+++ networking/initiate_network.py
@@ -25,4 +25,3 @@ network.edge["node1"]["node2"]["myattribute"] = 321
 print(network.edge["node2"]["node1"]["myattribute"])
 # adding an edge.
 #Specifying the network we want to save.
-network.add_edge("hello", "world")
