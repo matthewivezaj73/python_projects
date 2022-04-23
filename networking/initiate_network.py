82a380b0e8ba81b881c157eeaf5e85eb7c7d9264
matthewivezaj73 <81784356+matthewivezaj73@users.noreply.github.com>
2022-04-23 18:27:02 -0400

    Added an attribute.

--- networking/initiate_network.py
+++ networking/initiate_network.py
@@ -16,3 +16,4 @@ network.add_node(1,myattribute="foo")
 #Accessing the node.
 print(network.nodes[1]["myattribute"])
 #Adding an attribute to the edges.
+network.add_edge["node1"]["node2"]["myattribute"] = "attribute of an edge"
