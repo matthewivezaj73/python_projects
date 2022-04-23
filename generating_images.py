642718e5f32d53f0b9d4f5b97ae8aae1315d4580
matthewivezaj73 <81784356+matthewivezaj73@users.noreply.github.com>
2022-04-23 15:03:18 -0400

    commit

---
+++ generating_images.py
@@ -0,0 +1,9 @@
+#Importing libraries for data science
+import cv2
+import numpy as np
+import matplotlib.pyplot as plt
+#%matplotlib inline
+from IPython.display import Image
+
+# Display 18x18 pixel image.
+Image(filename='checkerboard_18x18.png') 
