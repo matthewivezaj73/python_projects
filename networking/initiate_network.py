7f34fe171e9b49dcf02469bb7033b5aece1bcb3d
matthewivezaj73 <81784356+matthewivezaj73@users.noreply.github.com>
2022-04-23 21:12:21 -0400

    Replaced code.

--- networking/initiate_network.py
+++ networking/initiate_network.py
@@ -1,29 +1,15 @@
-#Importing a library.
+#Importing libraries.
 import networkx
+#Importing a function.
 from networkx.drawing.nx_agraph import write_dot
-#Initiating a network with no node and no edges.
+
+# instantiate a network, add some nodes, and connect them
+nodes = ["hello","world",1,2,3]
 network = networkx.Graph()
-#Creating a list.
-nodes = ["hello", "world", 1, 2, 3]
-#Adding the nodes.
+#Printing out all of the nodes.
 for node in nodes:
     network.add_node(node)
-#Adding edges.
-network.add_edge("hello", "world")
-network.add_edge(1,2)
-network.add_edge(1,3)
-#Adding attributes to a node.
-network.add_node(1,myattribute="foo")
-#Accessing the node.
-print(network.nodes[1]["myattribute"])
-#Adding an attribute to the edges.
-network.add_edge("node1","node2",myattribute="attribute of an edge")
-#Adding attributes to edges once they've been added.
-network.edge["node1"]["node2"]["myattribute"] = "attribute of an edge"
-#Using the edge dictionary to access node attributes the other way around.
-network.edge["node1"]["node2"]["myattribute"] = 321
-print(network.edge["node2"]["node1"]["myattribute"])
-# adding an edge.
-network.add_edge("hello", "world")
-#Specifying the network we want to save.
-write_dot(network, "network.dot")
+#Adding an edge.
+network.add_edge("hello","world")
+#Saving the node.
+write_dot(network,"network.dot")
