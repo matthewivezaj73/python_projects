010f111fea657abc4d23ad2a5961f45c3805c6fc
matthewivezaj73 <81784356+matthewivezaj73@users.noreply.github.com>
2022-06-20 09:06:25 -0400

    Creating a class object

--- TicTacToe/tictactoe_main.py
+++ TicTacToe/tictactoe_main.py
@@ -4,6 +4,7 @@
 #Importing tictactoe from the class file, tictactoe.
 from classes.tictactoe import tictactoe
 #Creating a class object.
+new_tictactoe = tictactoe('Monty', 5, 5)
 #Setting flag to false.
 not_done = False
 #Testing for user input.
